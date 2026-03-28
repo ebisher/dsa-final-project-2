81780af9e91b5e329605dad4dff852624698de5f
andersonclara <andersonclara@ufl.edu>
2026-03-28 19:09:18 -0400

    renamed sort files correctly

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 //main for clara to implement
 #include "student_info.cpp"
-#include "sortandsearchalgorithms.cpp"
+#include "merge_sort.cpp"
+#include "quick_sort.cpp"
 #include <cstdlib>
 #include <ctime>
 #include <random>
@@ -56,39 +57,103 @@ int main() {
     //welcome heading display
 
     printHeader("SCHOOL MANAGEMENT SYSTEM");
-    std::cout << "Total Students: 100,000"<< std::endl << "Average GPA: " << GPA_avg << std::endl << std::endl;
+    std::cout << "Total Students: " << system.size() << std::endl << "Average GPA: " << GPA_avg << std::endl << std::endl;
 
-    //main menu
-    printHeader("Main Menu");
-    std::cout << "1. Student Records" << std::endl;
-    std::cout << "2. Sorting" << std::endl;
-    std::cout << "3. View Summary" << std::endl;
-    std::cout << "4. Exit" << std::endl;
+    while(true){
+        //main menu
+        printHeader("Main Menu");
+        std::cout << "1. Student Records" << std::endl;
+        std::cout << "2. Sorting" << std::endl;
+        std::cout << "3. View Summary" << std::endl;
+        std::cout << "4. Exit" << std::endl;
 
-    //Student records menu
-    printHeader("Student Records");
-    std::cout << "1. View all students" << std::endl;
-    std::cout << "2. Add student" << std::endl;
-    std::cout << "3. Update student" << std::endl;
-    std::cout << "4. Delete student" << std::endl;
-    std::cout << "5. Return to Main Menu" << std::endl;
+        int menuSelection;
+        std::cin >> menuSelection;
 
-    // sorting menu
-    printHeader("Sorting");
-    std::cout << "1. Merge sort by GPA" << std::endl;
-    std::cout << "2. Heap sort by GPA" << std::endl;
-    std::cout << "3. View honors roll" << std::endl;
-    std::cout << "4. Filter by class year" << std::endl;
-    std::cout << "5. Return to Main Menu" << std::endl;
+        if (menuSelection == 1){
+            //Student records menu
+            printHeader("Student Records");
+            std::cout << "1. View all students" << std::endl;
+            std::cout << "2. Add student" << std::endl;
+            std::cout << "3. Update student" << std::endl;
+            std::cout << "4. Delete student" << std::endl;
+            std::cout << "5. Return to Main Menu" << std::endl;
+
+            int subMenuSelection;
+            std::cin >> subMenuSelection;
+            
+            if (subMenuSelection == 1){ //prints out all students
+                system.listAll();
+
+            } else if (subMenuSelection == 2){ //adds student with user input info
+                std::cout << "Add student below: ";
+                Student newStudent = inputStudent();
+                system.addStudent(newStudent);
+                std::cout << "  Student '" << newStudent.name << "' added successfully.\n";
+
+            } else if (subMenuSelection == 3){ //updates student info
+                std::cout << "Enter ID to update: ";
+                int id;
+                std::cin >> id;
+                Student updatedStudent = inputStudent();
+                updatedStudent.id = id;
+                system.updateStudent(id, updatedStudent);
+
+            } else if (subMenuSelection == 4){ //delete student
+                std::cout << "Enter ID to delete: ";
+                int id;
+                std::cin >> id;
+                system.deleteStudent(id);
+                
+            } else if (subMenuSelection == 5){ //return to main menu
+                continue;
+
+            } else { //invalid selection catch
+                std::cout << "Invalid selection." << std::endl;
+            }
+
+        } else if (menuSelection == 2)
+        {
+            // sorting menu
+            printHeader("Sorting");
+            std::cout << "1. Merge sort by GPA" << std::endl;
+            std::cout << "2. Heap sort by GPA" << std::endl;
+            std::cout << "3. View honors roll" << std::endl;
+            std::cout << "4. Filter by class year" << std::endl;
+            std::cout << "5. Return to Main Menu" << std::endl;
+      
+            int subMenuSelection;
+            std::cin >> subMenuSelection;
+            
+            if (subMenuSelection == 1){ //call merge sort by gpa
+
+            } else if (subMenuSelection == 2){ //call heap sort by gpa
 
 
-    // //summary screen TODO
-    // std::cout << "========================================" << std::endl;
-    // std::cout << "      Main Menu";
-    // std::cout << "1. Student Records";
-    // std::cout << "2. Sorting";
-    // std::cout << "3. View Summary";
-    // std::cout << "4. Exit";
+            } else if (subMenuSelection == 3){ // view honor roll
+                system.showHonorsRoll();
+                
+            } else if (subMenuSelection == 4){ // filter by year
+                std::cout << "Enter class year to filter by: ";
+                int yearFilter;
+                std::cin >> yearFilter;
+                system.showClassYear(yearFilter);
+                
+            } else if (subMenuSelection == 5){ //return to main menu
+                continue;
 
+            } else { //invalid selection catch
+                std::cout << "Invalid selection." << std::endl;
+            }            
+        
+        } else if (menuSelection == 3){
+            //summary screen TODO
+        } else if (menuSelection == 4){ //exit program
+            std::cout << "Thank you for using DK's School Management System. Goodbye!";
+            break;
+        } else { //invalid selection catch
+            std::cout << "Invalid selection." << std::endl;
+        }
+    }
     return 0;
 }
